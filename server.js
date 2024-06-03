52d60812eb459c67b052b0ff9d2109200b63da85
ononoki0117 <tytttyty@naver.com>
2024-06-03 13:19:34 +0900

    nodemon added

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 const express = require("express");
+
+const app = express();
+
+const port = 8080;
+
+const path = require("path");
+
+app.use(express.static(path.resolve((path.join(__dirname, 'main/src')))));
+
+app.listen(port, ()=>{
+    console.log('app started');
+})
+
+app.get('/', function(req, res){
+    res.sendFile(path.resolve('./main/main/index.html'));
+});
+
