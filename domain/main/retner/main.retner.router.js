5fcc8e453ad4c108a7ee846d9027d450b7e83b3d
ononoki0117 <111170868+ononoki0117@users.noreply.github.com>
2024-06-19 16:26:48 +0900

    프론트 작업해주세요

--- domain/main/retner/main.retner.router.js
+++ domain/main/retner/main.retner.router.js
@@ -4,4 +4,10 @@ const controller = require('./main.retner.controller');
 router.route('/retner/main')
     .get(controller.getRetnerMainPage);
 
+router.route('/retner/chats')
+    .get(controller.getRetnerChatList);
+
+router.route('/retner/calendar')
+    .get(controller.getRetnerCallender);
+
 module.exports = router;
